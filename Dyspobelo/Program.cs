c62afcbcd45957bda72ab0972a82a148990d0aa1
s22540 <s22540@pjwstk.edu.pl>
2024-03-27 18:55:23 +0100

    Aktualizacja metody połączenia z bazą danych

--- Dyspobelo/Program.cs
+++ Dyspobelo/Program.cs
@@ -6,8 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Pobieranie connection string z zmiennej œrodowiskowej
+var azureMySqlConnectionString = Environment.GetEnvironmentVariable("AZURE_MYSQL_CONNECTIONSTRING");
+
+
 builder.Services.AddDbContext<MyDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("MyDatabase"), new MySqlServerVersion(new Version(8, 0, 21))));
+    options.UseMySql(azureMySqlConnectionString, new MySqlServerVersion(new Version(8, 0, 21))));
 
 var app = builder.Build();
 
@@ -15,10 +19,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();

7fe9e557a24ac23cb3f607b9fff758950780b4d3
s20178 <s20178@pjwstk.edu.pl>
2024-04-15 21:03:21 +0200

    routes

--- Dyspobelo/client-app/src/App.js
+++ Dyspobelo/client-app/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import LoginScreen from "./screens/login";
 import MainScreen from "./screens/mainScreen";
 import AddAnnouncement from "./screens/AddAnnouncement";
+// import EditAnnouncement from "./screens/EditAnnouncement";
+// import ShowAnnouncement from "./screens/ShowAnnouncement";
+// import Settings from "./screens/Settings";
 
 function App() {
 	return (
@@ -11,6 +14,9 @@ function App() {
 				<Route path="/login" element={<LoginScreen />} />
 				<Route path="/main" element={<MainScreen />} />
 				<Route path="/add-announcement" element={<AddAnnouncement />} />
+				{/* <Route path="/edit-announcement" element={<EditAnnouncment />} />
+				<Route path="/show-announcement" element={<ShowAnnouncement />} />
+				<Route path="/settings" element={<Settings />} /> */}
 				<Route path="/" element={<Navigate to="/login" replace />} />
 			</Routes>
 		</BrowserRouter>

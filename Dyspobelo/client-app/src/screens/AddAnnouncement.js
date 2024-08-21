0f7136ed4972efec28a4730b7248e1c3bfd71948
Piotr Lubak <s22511@pjwstk.edu.pl>
2024-08-21 20:38:44 +0200

    Poprawki graficzne

--- Dyspobelo/client-app/src/screens/AddAnnouncement.js
+++ Dyspobelo/client-app/src/screens/AddAnnouncement.js
@@ -2,62 +2,76 @@
 import { useTranslation } from "react-i18next";
 import Form from "../components/Form";
 import Menu from "../components/Menu";
-import MovingMarkerLogic from "../components/MovingMarkerLogic";
 import MapComponent from "../components/MapComponent";
 
 const AddAnnouncement = ({ children }) => {
-	const movingMarkerRef = useRef(null);
-	const { t } = useTranslation();
+    const movingMarkerRef = useRef(null);
+    const { t } = useTranslation();
 
-	const handleNewReport = (coordinates, vehicleId) => {
-		console.log(
-			"handleNewReport called with coordinates:",
-			coordinates,
-			"and vehicleId:",
-			vehicleId
-		);
+    const handleNewReport = (coordinates, vehicleId) => {
+        console.log(
+            "handleNewReport called with coordinates:",
+            coordinates,
+            "and vehicleId:",
+            vehicleId
+        );
 
-		if (movingMarkerRef.current) {
-			console.log("Calling handleNewReport on MovingMarkerLogic");
-			movingMarkerRef.current.handleNewReport(coordinates, vehicleId);
-		} else {
-			console.log("movingMarkerRef.current is null");
-		}
-	};
+        if (movingMarkerRef.current) {
+            console.log("Calling handleNewReport on MovingMarkerLogic");
+            movingMarkerRef.current.handleNewReport(coordinates, vehicleId);
+        } else {
+            console.log("movingMarkerRef.current is null");
+        }
+    };
 
-	const styles = {
-		layout: {
-			display: "flex",
-			justifyContent: "space-between",
-			alignItems: "flex-start",
-		},
-		container: {
-			width: "100%",
-		},
-		halfWidth: {
-			width: "50%",
-		},
-	};
-	return (
-		<div style={styles.container}>
-			<div>
-				<Menu />
-			</div>
-			<div style={styles.layout}>
-				<div style={styles.halfWidth}>
-					<Form onReportSubmit={handleNewReport} />
-				</div>
-				<div style={styles.halfWidth}>
-					{/* Jeśli dzieci (children) są przekazane, użyj ich, w przeciwnym razie użyj MapComponent */}
-					{children ? (
-						React.cloneElement(children, { ref: movingMarkerRef })
-					) : (
-						<MapComponent ref={movingMarkerRef} />
-					)}
-				</div>
-			</div>
-		</div>
-	);
+    const styles = {
+        layout: {
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-start",
+            width: "100%",
+            overflow: "hidden",
+            boxSizing: "border-box",
+        },
+        container: {
+            width: "100%",
+            padding: "20px",
+            boxSizing: "border-box",
+            overflowX: "hidden",
+            overflowY: "auto",
+        },
+        halfWidth: {
+            width: "48%",
+            boxSizing: "border-box",
+            height: "60vh",
+        },
+        mapContainer: {
+            marginRight: "20px",
+            width: "100%",
+            boxSizing: "border-box",
+            height: "100%",
+        }
+    };
+
+    return (
+        <div style={styles.container}>
+            <Menu />
+            <div style={styles.layout}>
+                <div style={styles.halfWidth}>
+                    <Form onReportSubmit={handleNewReport} />
+                </div>
+                <div style={styles.halfWidth}>
+                    <div style={styles.mapContainer}>
+                        {children ? (
+                            React.cloneElement(children, { ref: movingMarkerRef })
+                        ) : (
+                            <MapComponent ref={movingMarkerRef} />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default AddAnnouncement;
--- Dyspobelo/client-app/src/screens/mainScreen.js
+++ Dyspobelo/client-app/src/screens/mainScreen.js
@@ -5,20 +5,38 @@ import Menu from "../components/Menu";
 const MainScreen = ({ children }) => {
     const { t } = useTranslation();
     const styles = {
+        html: {
+            height: "100%",
+            margin: 0,
+            padding: 0,
+        },
+        body: {
+            height: "100%",
+            margin: 0,
+            padding: 0,
+            overflow: "hidden", 
+        },
         outerContainer: {
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            height: "100vh",
+            justifyContent: "center",
+            overflow: "hidden",
+            margin: 0,
+            padding: 0,
         },
         mapContainer: {
             width: "80%",
             display: "flex",
             justifyContent: "center",
+            margin: 0,
+            padding: 0,
         },
     };
 
     return (
-        <div>
+        <div style={styles.body}>
             <Menu />
             <div style={styles.outerContainer}>
                 <div style={styles.mapContainer}>{children}</div>
